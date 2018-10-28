d604670f66bf89af80f8ac87e1df0011d76e6d89
Alistair Boettiger <boettiger.alistair@gmail.com>
2018-10-28 15:19:12 -0700

    switched theme to minimal-mistakes-jekyll

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory - edit this post and re-build (or run with the -w switch) to see your changes!To add new posts, simply add a file in the _posts directory that follows the convention: YYYY-MM-DD-name-of-post.ext. Jekyll also offers powerful support for code snippets: def print_hi(name)...","categories": ["main"],
+        "tags": [],
+        "url": "/main/2016/02/12/sample-content.html",
+        "teaser":null}]
